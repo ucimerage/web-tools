1de7baa681b86bb375bbfb15eb5275c2b1c9e26a
Jeff Feng <MrJeffFeng@users.noreply.github.com>
2018-05-09 13:21:35 -0700

    V2 - New look plus improved UI
    Added:
    New CSS styles
    Event Table Creator (beta)
    Drop-down tool select
    
    Removed:
    Duplicate textareas and buttons

--- custom.js
+++ custom.js
@@ -1,11 +1,18 @@
+function updateAction(action) {
+  actions = { rescol: ['Research Colloquia', 'rescol()'], linker: ['Program Linker', 'linker()'], EventTable: ['Event Table', 'eventTable()']}
+  document.getElementById('input').value = '';
+  document.getElementById('input').setAttribute( "onkeyup", actions[action][1] );
+  document.getElementById('tool-name').innerHTML = actions[action][0]
+}
+
 function rescol() {
-  var colloquim = document.getElementById('rescol').value;
+  var colloquim = document.getElementById('input').value;
 
   var host = '';
   var date = '';
   var output = '';
   var days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  var lines = colloquim.split('\n');
+  var lines = breakLine(colloquim);
 
   for (i=0; i<lines.length; i++) {
     if (lines[i].startsWith('(Host')) {
@@ -37,7 +44,8 @@ function rescol() {
       }
     }
   }; //
-  document.getElementById('col_result').value = output;
+  document.getElementById('result').value = output;
+  document.getElementById('result').select();
 };;
 
 function linker() {
@@ -46,7 +54,7 @@ function linker() {
                       'Health Care Executive MBA': 'https://merage.uci.edu/programs/masters/healthcare-executive-mba/index.html',
                       'Executive MBA': 'https://merage.uci.edu/programs/masters/executive-mba/index.html',};
 
-  var str = document.getElementById('linker').value;
+  var str = document.getElementById('input').value;
 
   for (var key in program_list) {
     var re = new RegExp(`(${key})(?![^<a.+>]*<\/a>)`);
@@ -55,5 +63,46 @@ function linker() {
     }
   }
 
-  document.getElementById('link_result').value = str;
+  document.getElementById('result').value = str;
+  document.getElementById('result').select();
 };;
+
+function eventTable() {
+  var schedule = breakLine(document.getElementById('input').value);
+  var time = [];
+  var info = [];
+  for (i=0; i<schedule.length; i++) {
+    var parse = schedule[i].split(' ');
+    var index = 0;
+    for (x=0; x<parse.length; x++) {
+      if (parse[x].match(/[0-9](am)/g) || parse[x].match(/[0-9](pm)/g)) {
+        index = schedule[i].indexOf(parse[x]) + parse[x].length;
+      }
+    }
+    time.push(schedule[i].substring(0, index));
+    info.push(schedule[i].substring(index + 1));
+  };
+  document.getElementById('result').value = scheduleTable(time, info)
+  document.getElementById('result').select();
+};;
+
+function breakLine(str) {
+  return str.split('\n')
+};;
+
+function scheduleTable(time, info) {
+  var table = ['<table align="center" class="table-striped table" style="width: 100%;">', '<tbody>', '</tbody>', '</table>']
+  for (i=0; i<time.length; i++) {
+    table.splice(-2, 0, '<tr>\n<td style="text-align: right;" width="25%">'+time[i]+'</td>\n<td>'+info[i]+'</td>\n</tr>')
+  }
+  var tableOutput = formatHTML(table);
+  return tableOutput
+};;
+
+function formatHTML(arr) {
+  var output = '';
+  for (i=0; i<arr.length; i++) {
+    output = output + arr[i] + '\n'
+  }
+  return output
+}
